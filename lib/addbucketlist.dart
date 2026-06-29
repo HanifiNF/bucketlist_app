aaf61f46e2134011e71505bbdb825a95dcc5de7b
Hanifi Abrar Setiawan <hanifisetiawan@gmail.com>
2026-06-29 15:09:32 +0700

    initial commit

---
+++ lib/addbucketlist.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Addbucketlistscreen extends StatefulWidget {
+  const Addbucketlistscreen({super.key});
+
+  @override
+  State<Addbucketlistscreen> createState() => _AddbucketlistscreenState();
+}
+
+class _AddbucketlistscreenState extends State<Addbucketlistscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Add Bucket List")));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bucketlist_app/addbucketlist.dart';
 import 'package:bucketlist_app/mainscreen.dart';
 import 'package:flutter/material.dart';
 
@@ -16,8 +17,16 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        "/home": (context) {
+          return MainScreen();
+        },
+        "/add": (context) {
+          return Addbucketlistscreen();
+        },
+      },
+      initialRoute: "home",
       theme: ThemeData.light(useMaterial3: true),
-      home: MainScreen(),
     );
   }
 }
